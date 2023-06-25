5fe6468642140a6ba3d000daaa30d6d9e2415cec
minetake <minetake01.m@gmail.com>
2023-06-25 13:28:14 +0900

    Strapiのマウントするディレクトリを必要な部分だけに変更

--- strapi-docker/docker-entrypoint.sh
+++ strapi-docker/docker-entrypoint.sh
@@ -3,17 +3,9 @@ set -eu
 
 cd /srv/app
 
-if [ ! -f "package.json" ]; then
-    yarn create strapi-app . --no-run --quickstart
-fi
+eval $CUSTOM_COMMAND
 
-if [ ! -d "node_modules" ]; then
-    yarn install --prod
-fi
-
-if [ ! -d "node_modules/mysql2" ]; then
-    yarn add mysql2
-fi
+mkdir -p public/uploads
 
 if [ $NODE_ENV = "develop" ]; then
     exec yarn strapi develop
