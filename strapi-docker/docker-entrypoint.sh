b22f2499ab8d6c25a5502f5427f3e44d4c162556
minetake01 <minetake01.m@gmail.com>
2023-06-28 07:40:53 +0900

    quickstartを使用せずにstrapiをインストールするように変更

--- strapi-docker/docker-entrypoint.sh
+++ strapi-docker/docker-entrypoint.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 set -eu
 
+if [ ! -f "/srv/app/package.json" ]; then
+    mkdir /strapi_tmp
+    cd /strapi_tmp
+    yarn create strapi-app . --no-run \
+        --dbclient=mysql \
+        --dbhost=$DATABASE_HOST \
+        --dbport=$DATABASE_PORT \
+        --dbname=$DATABASE_NAME \
+        --dbusername=$DATABASE_USERNAME \
+        --dbpassword=$DATABASE_PASSWORD
+    yarn install --prod
+    yarn add mysql2
+    mv -n /strapi_tmp/* /srv/app
+    rm -r /strapi_tmp
+fi
+
 cd /srv/app
 
 eval $CUSTOM_COMMAND
+
+yarn build
 
 mkdir -p public/uploads
 
